bad633d0ae99f792e4b5b37d64feb8109fa076bc
Sherzod <sherzodotajonovumirbekovich@gmail.com>
2021-01-01 10:53:22 +0100

    Add a falder called general stuff where i upload some algorithms and stuff i used for a problem and i think i might need again later

---
+++ General_stuff/get_stuff_inside_brackets.py
@@ -0,0 +1,21 @@
+def match_brackets():
+    s = input()
+    flag = False
+    opens = 0
+    start_idx = end_idx = 0
+    for i in range(len(s)):
+        if s[i] == '(' and not flag:
+            opens += 1
+            flag = True
+            start_idx = i+1
+        elif s[i] == '(' and flag:
+            opens += 1
+        elif s[i] == ')':
+            opens -= 1
+
+        if flag and opens == 0:
+            flag = False
+            end_idx = i
+    return s[start_idx: end_idx]
+
+print(match_brackets())
