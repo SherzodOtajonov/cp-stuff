708b739636900f36ea2daaea980671b9b77b15d0
Sherzod <sherzodotajonovumirbekovich@gmail.com>
2021-03-11 19:21:54 +0100

    started make a templete for cp

---
+++ general_stuff/cpp_templete.cpp
@@ -0,0 +1,11 @@
+// started making a cpp templete
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+
+constexpr int MOD = 1e9+7;
+
