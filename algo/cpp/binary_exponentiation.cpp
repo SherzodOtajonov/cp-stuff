aa183446c79bed0dfa99d32676bdf8735cea95d5
Sherzod <sherzodotajonovumirbekovich@gmail.com>
2021-04-06 20:27:04 +0500

    update binary expo

--- algo/cpp/binary_exponentiation.cpp
+++ algo/cpp/binary_exponentiation.cpp
@@ -5,19 +5,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define endl '\n'
+#define ll long long
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+
+typedef pair<int, int> pairs;
+
+constexpr int MOD = 1e9+7;
+
+ll mod(ll n, ll m=MOD){n%=m;if(n<0)n+=m;return n;}
+
 // a>0, b>=0
-long long binpow(long long a, long long b, long long mod) {
-    a%=mod;
-    long long res = 1;
-    while (b) {
-        if (b%2) res = res*a%mod;
-        a=a*a%mod;
+ll binpow(ll a, ll b) {
+    a=mod(a);
+    ll res = 1;
+    while(b) {
+        if(b%2) res = mod(res*a);
+        a=mod(a*a);
         b/=2;
     }
     return res;
 }
 
 int main(){
-    // cout<<binpow(2, 704511, 1000000007)<<endl; // 852098711
+
     return 0;
 }
